2a0c8c019f58a44ac0cb6f737ab4f8062aee7e69
Josh Sommer <JoshDSommer@gmail.com>
2016-07-11 21:33:55 -0400

    addresses height of navigation bar being to large.

--- nativescript-swiss-army-knife.ts
+++ nativescript-swiss-army-knife.ts
@@ -109,6 +109,7 @@ export class SwissArmyKnife {
 			let resourceId = app.android.currentContext.getResources().getIdentifier('navigation_bar_height', 'dimen', 'android');
 			if (resourceId > 0) {
 				result = app.android.currentContext.getResources().getDimensionPixelSize(resourceId);
+				result = result / app.android.currentContext.getResources().getDisplayMetrics().density;
 			}
 			return result;
 		} else {

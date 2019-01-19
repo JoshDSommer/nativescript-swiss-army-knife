9f2f02c816e4860dabe136558a44bdf63615250e
Josh Sommer <JoshSo@Joshs-MacBook-Pro.local>
2019-01-19 11:38:11 -0500

    updating tsconfig to emit d.ts files

--- src/nativescript-swiss-army-knife.d.ts
+++ src/nativescript-swiss-army-knife.d.ts
@@ -4,36 +4,36 @@ import { View } from "tns-core-modules/ui/core/view";
 import { LayoutBase } from "tns-core-modules/ui/layouts/layout-base";
 import { ListView } from "tns-core-modules/ui/list-view";
 import { ScrollView } from "tns-core-modules/ui/scroll-view";
-
-export declare class SwissArmyKnife {
-	android: any;
-	ios: any;
-	static pluckChildViewsFromLayout(parent: LayoutBase): Array<View>;
-	static disableScrollBounce(view: ScrollView | ListView): void;
-	static removeHorizontalScrollBars(view: ScrollView | ListView): void;
-	static removeVerticalScrollBars(view: ScrollView | ListView): void;
-	static getScreenHeight(): IScreenHeight;
-	static dismissSoftKeyboard(): void;
-	static actionBarSetTitle(title: string): void;
-	static actionBarAddButton(button: ActionItem): void;
-	static actionBarClearButtons(): void;
-	static setAndroidStatusBarTranslucentFlag(): void;
-	static setAndroidStatusBarColor(color: string | Color): void;
-	static setAndroidNavBarColor(color: string | Color): void;
-	static resetAndroidStatusBarTranslucentFlag(): void;
-	static setAndroidNavBarTranslucentFlag(): void;
-	static resetAndroidNavBarTranslucentFlag(): void;
-	static actionBarHideBackButton(): void;
-	static actionBarSetStatusBarStyle(style: number): void;
-
-	private static getBarColor(color);
-	private static getStatusBarHeight();
-	private static getNavBarHeight();
-}
-
 export interface IScreenHeight {
 	portrait: number;
 	landscape: number;
 	androidStatusBar: number;
 	androidNavBar: number;
+}
+export declare class SwissArmyKnife {
+	readonly android: any;
+	readonly ios: any;
+	static getListViewVerticalOffset(view: ListView): number;
+	static disableScrollBounce(view: ScrollView | ListView): void;
+	static removeHorizontalScrollBars(view: ScrollView | ListView): void;
+	static removeVerticalScrollBars(view: ScrollView | ListView): void;
+	static pluckChildViewsFromLayout(parent: LayoutBase): Array<View>;
+	static getScreenHeight(): IScreenHeight;
+	static actionBarSetTitle(title: string): void;
+	static actionBarAddButton(button: ActionItem): void;
+	static actionBarClearButtons(): void;
+	static setAndroidStatusBarTranslucentFlag(): void;
+	static resetAndroidStatusBarTranslucentFlag(): void;
+	static setAndroidNavBarTranslucentFlag(): void;
+	static resetAndroidNavBarTranslucentFlag(): void;
+	static setAndroidStatusBarColor(color: string | Color): void;
+	static setAndroidNavBarColor(color: string | Color): void;
+	static actionBarHideBackButton(): void;
+	static actionBarSetStatusBarStyle(style: number): void;
+	static dismissSoftKeyboard(): void;
+	private static _getStatusBarHeight;
+	private static _getBarColor;
+	private static _getNavBarHeight;
+	private static readonly _androidContext;
+	private static readonly _androidActivity;
 }
